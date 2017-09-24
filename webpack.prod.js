0645648565f865d6c728520dbda42f54ab4df7dd
Zarni Hpyoe <zarnihpyoe@gmail.com>
2017-09-24 02:02:52 -0400

    7.2 Shorthand for minifying and adding source map

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,8 @@
-const webpack = require('webpack')
 const merge = require('webpack-merge')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     devtool: 'source-map',
-    plugins: [
-        new UglifyJSPlugin({
-            sourceMap: true,
-        }),       // minify output bundle
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production'),
-            },
-        }),
-    ]
+    // webpack -p = webpack --optimize-minize --define process.env.NODE_ENV="'production'"
 })
