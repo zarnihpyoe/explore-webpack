97970736575370c0d041f52891bc93f8983b73e2
Zarni Hpyoe <zarnihpyoe@gmail.com>
2017-09-24 00:19:06 -0400

    3. Manage output

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin([ 'dist' ]),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
