b910a3ab88fcc73a9684f04e3c3172025d46a995
Zarni Hpyoe <zarnihpyoe@gmail.com>
2017-09-24 00:53:54 -0400

    4.1 Add Hot Module Replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
     entry: {
         app: './src/index.js',
-        print: './src/print.js',
     },
     devtool: 'inline-source-map',       // source map for easier debug (don't do this for production)
     devServer: {                        // telling webpack-dev-server
         contentBase: './dist',            // - to serve files from ./dist on localhost:8080
+        hot: true,                        // - to use hot module replacement
     },
     plugins: [
         new CleanWebpackPlugin([ 'dist' ]),
         new HtmlWebpackPlugin({
             title: 'Output Management'
         }),
+        new webpack.HotModuleReplacementPlugin(),
     ],
     output: {
         filename: '[name].bundle.js',
