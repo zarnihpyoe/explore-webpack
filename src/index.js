2933d737525a349abb2ae9b6bffa77d64abc405b
Zarni Hpyoe <zarnihpyoe@gmail.com>
2017-09-23 23:58:43 -0400

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import _ from 'lodash'
 
-import './style.css'
-import Icon from './icon.svg'
-
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    var myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
     return element;
 }
   
